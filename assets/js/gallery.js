2bbbd7469eb0a0498232bc43b256a2deba108904
Dalton Kirkpatrick <d.kirkpatrick99@gmail.com>
2021-12-06 11:44:10 -0600

    Added new project

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,29 +1,54 @@
+
 const panels = document.querySelectorAll('.panel');
+const panelContainer = document.querySelector('.panels')
 
-    function toggleOpen() {
-      this.classList.toggle('open');
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+function classCheck() {
+  if((this.classList.contains("open") && this.classList.contains("open-active")) || (!this.classList.contains("open") && !this.classList.contains("open-active"))) {
+    let panelsArr = [...panels]
+    let check = panelsArr.some(panel => {
+      let list = [...panel.classList]
+      if (list.includes('open-active') && list.includes('open')) return true
+    })
+
+    if (check) {
+      // panelContainer.style.height = '80vh'
+      panelContainer.classList.add('open')
+    } else {
+      // panelContainer.style.height = ''
+      panelContainer.classList.remove('open')
     }
+  } 
+  // else {
+  //   this.classList.remove("open");
+  //   this.classList.remove("open-active");
 
-    function classCheck() {
-      if((this.classList.contains("open") && this.classList.contains("open-active")) || (!this.classList.contains("open") && !this.classList.contains("open-active"))) {
-        return;
-      } else {
-        this.classList.remove("open");
-        this.classList.remove("open-active");
+  //   let panelsArr = [...panels]
+  //   let check = panelsArr.some(panel => {
+  //     let list = [...panel.classList]
+  //     if (list.includes('open-active') && list.includes('open')) return true
+  //   })
 
-      }
-    }
+  //   if (check) {
+  //     panelContainer.style.height = '80vh'
+  //   } else {
+  //     panelContainer.style.height = ''
+  //   }
+  // }
+}
 
-    function toggleActive(e) {
-      console.log(e.propertyName);
-      if (e.propertyName.includes('flex')) {
-        this.classList.toggle('open-active');
-      }
-    }
+function toggleActive(e) {
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
 
-    // panels.forEach(panel => panel.addEventListener('click', closeAll));
+// panels.forEach(panel => panel.addEventListener('click', closeAll));
 
-    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-    // panels.forEach(panel => panel.removeEventListener('click', toggleOpen));
-    panels.forEach(panel => panel.addEventListener('transitionstart', toggleActive));
-    panels.forEach(panel => panel.addEventListener('transitionend', classCheck));
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+// panels.forEach(panel => panel.removeEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionstart', toggleActive));
+panels.forEach(panel => panel.addEventListener('transitionend', classCheck));
